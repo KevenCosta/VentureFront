b9f8115af3710da0fb99a95ae466a537531109c8
KevenCostaNote <kevencosta129@gmail.com>
2022-02-24 21:56:39 -0300

    Landing Page add

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div classname="banner">
+        <div className='venture-title'>
+          Venture
+        </div>
+        <div className='class-title'>
+          Class
+        </div>
+      </div>
+      <div className='content'>
+        <div className='content-text'>
+          <h2>Educação que</h2>
+          <h2 id='content-secondary'>transforma</h2>
+          <p>Alunos de todo o mundo estão iniciando novas carreiras e avançando em suas áreas.</p>
+          <div>
+            <button className='button-training'>Iniciar treinamento</button>
+          </div>
+        </div>
+        <div className='content-img'>
+          <img src={require('./img/cao1.png')} alt="img_dog" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
